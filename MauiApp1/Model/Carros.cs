f1995e6a45b40eec9233fda8741f68a450754da3
Vinni <seuemail@exemplo.com>
2024-12-10 20:38:47 -0300

    first commit

---
+++ MauiApp1/Model/Carros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Model
+{
+    public class Carros
+    {
+        public string Name { get; set; }
+        public float Acceleration { get; set; }
+        public string Year { get; set; }
+        public string Origin { get; set; }
+        public string Image { get; set; }
+    }
+}
